83478ffe6ff38e93308553c1429aab7ab0d5d562
zhaodongsheng <zhaodongsheng@lean-soft.com>
2017-10-11 17:39:46 +0800

    添加项目文件。

---
+++ NetCore2.EFCore2.CodeFirstCreateDataBaseDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCore2.EFCore2.CodeFirstCreateDataBaseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
